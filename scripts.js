95deef7581eb8c6541c570a70b7a1fbb2da4382e
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2023-07-26 10:59:06 -0400

    Added modal

--- scripts.js
+++ scripts.js
@@ -1,9 +1,24 @@
 let myLibrary = [];
 
+const modal = document.querySelector('#modal');
+const addBook = document.querySelector('#add-book');
+const submitButton = document.querySelector('.submit-button');
+
+addBook.addEventListener("click", () => {
+    modal.showModal();
+});
+
+submitButton.addEventListener('click', () => {
+    modal.close();
+});
+
 function Book() {
   // the constructor...
 }
 
 function addBookToLibrary() {
-  // do stuff here
+    myLibrary.forEach(element => {
+        console.log(element)
+    });
 }
+
